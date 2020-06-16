d55ad253e66b91bb8c47361a93677cf9854159de
arthur2606 <56992658+arthur2606@users.noreply.github.com>
2020-06-17 00:57:21 +0200

    ajout du fichier cpp pour affichage et gestion du score

---
+++ score.cpp
@@ -0,0 +1,21 @@
+#include "score.h"
+#include <QFont>
+
+Score::Score(QGraphicsItem *parent): QGraphicsTextItem(parent){
+
+    // intialisation du score
+    score = 0;
+
+    // gestion de l'affichage du score
+    setPlainText(QString("Score: ") + QString::number(score)); // Score: 0
+    setDefaultTextColor(Qt::blue);
+    setFont(QFont("times",16));
+}
+
+void Score::increase(){
+    score++;
+    setPlainText(QString("Score: ") + QString::number(score)); // Score: 1
+}
+
+int Score::getScore(){
+    return
