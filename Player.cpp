b1a536cca39a7862306404d36f28356b5b22c4e9
arthur2606 <56992658+arthur2606@users.noreply.github.com>
2020-06-23 14:29:11 +0200

    player update

---
+++ Player.cpp
@@ -0,0 +1,31 @@
+#include "Player.h"
+#include <QGraphicsScene>
+#include <QKeyEvent>
+#include "Shoot.h"
+#include "Enemy.h"
+
+#include <QDebug>
+
+void Player::keyPressEvent(QKeyEvent *event){
+    if (event->key() == Qt::Key_Left){
+        if (pos().x() > 0)
+        setPos(x()-60,y());
+    }
+    else if (event->key() == Qt::Key_Right){
+        if (pos().x() + 100 < 800)
+        setPos(x()+60,y());
+    }
+    else if (event->key() == Qt::Key_Space){
+        // creation de la balle
+        Shoot * shoot = new Shoot();
+        shoot->setPos(x(),y());
+        scene()->addItem(shoot);
+
+          }
+}
+
+void Player::spawn(){
+    // creation de l'ennemi
+    Enemy * enemy = new Enemy();
+    scene()->addItem(enemy);
+}
