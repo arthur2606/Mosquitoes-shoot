9e0c4457540bc8b8a255537fc450511d4832191d
Israel-hakoua <56992624+Israel-hakoua@users.noreply.github.com>
2020-06-24 04:02:55 +0200

    Constr Player + private

--- Player.h
+++ Player.h
@@ -4,13 +4,17 @@
 #include <QGraphicsPixmapItem>
 #include <QGraphicsRectItem>
 #include <QObject>
+#include <QMediaPlayer>
 
 class Player:public QObject, public QGraphicsPixmapItem{
     Q_OBJECT
 public:
+    Player(QGraphicsPixmapItem *parent1=0);
     void keyPressEvent(QKeyEvent * event);
 public slots:
     void spawn();
+private:
+    QMediaPlayer * shootsound;
 };
 
 #endif // MYRECT_H
